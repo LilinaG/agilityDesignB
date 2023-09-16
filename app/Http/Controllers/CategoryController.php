fb61b803f812c731ac83cd0d81cd8f02de43586f
Liliana Gutierrez <lgutierrezfer@gmail.com>
2023-09-16 20:49:01 +0200

    CagetoryController added

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,57 +6,40 @@ use App\Models\Category;
 use App\Http\Requests\CategoryRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
-    public function index(): View 
+    public function index(): JsonResource
     {
-        $categories=Category::all();
-        return view('categories.index', compact('categories'));
+        return CategoryResource::collection(Category::all());
     }
 
-    public function create(): View   
+    public function create(): JsonResponse
     {
-        return view ('categories.create');
+        $category = Category::create([
+            "name_category"=> $request->name_category
+        ]);
+        return response()->json([
+            "status"=> true,
+       ], 200);
     }
-
-    public function store(CategoryRequest $request): RedirectResponse
-    {
-        Category::create($request -> all());
-        return redirect()->route('categories.index');
-    //     $data = $request->all();
     
-    // // Verificar si 'name_category' está presente en los datos del formulario
-    // if (isset($data['name_category'])) {
-    //     Category::create([
-    //         'name_category' => $data['name_category']
-    //     ]);
-    // } else {
-    //     // Puedes manejar aquí lo que sucede si 'name_category' no se proporciona
-    //     // Puedes mostrar un mensaje de error o tomar otras acciones según tus necesidades.
-    // }
+        public function store(CategoryRequest $request):JsonResponse
+        {
+                $category = Category::create($request->all());
+                return response()->json([
+                'success'=> true,
+                'data'=> $category //vigilar aquí que podría ser mediante objeto
+            ], 201);//201 significa estado de registro creado satisfactoriamente
+        }
 
-    // return redirect()->route('categories.index');
+        public function destroy(string $id):JsonResponse 
+        {
+            Category::find($id)->delete();
+            return response()->json([
+                'success'=> true
+            ], 200);
+        }
     }
-
-    public function edit(Category $category): View   
-    {
-        return view('categories.edit', compact('category'));
-    }
-
-    public function update(Request $request, Category $category): RedirectResponse
-    {
-        $category->update($request->all());
-        return redirect()->route('categories.index');
-    }
-
-    public function show(Category $category):View
-    {
-        return view('categories.show', compact('category'));
-    }
-    public function destroy(Category $category): RedirectResponse
-    {
-        $category->delete();
-        return redirect()->route('categories.index');
-    }
-}
+    
